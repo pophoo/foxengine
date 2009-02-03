3fb6ec43a1c4349949186539459d7508e23e55c6
wanfox <wanfox@471201e2-bed9-11dd-b7f4-ab9653f8cbb3>
2009-02-03 04:38:41 +0000

    设置raw和demo单独测试时的django环境

--- normal/demo_test.py
+++ normal/demo_test.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
+import os
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    #准备测试环境
+    from django.core.management import setup_environ
+    import wolfox.foxit.other_settings.settings_sqlite_test as settings
+    setup_environ(settings)
+
+
 from wolfox.fengine.normal.demo import *
+
+import logging
+logger = logging.getLogger('wolfox.fengine.normal.core.raw_test')
 
 class ModuleTest(unittest.TestCase):    #保持demo的有效性
     def test_demo(self):
@@ -22,5 +34,6 @@ class ModuleTest(unittest.TestCase):    #保持demo的有效性
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="test.log",level=logging.DEBUG,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
     unittest.main()
 
--- normal/raw_test.py
+++ normal/raw_test.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
+import os
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    #准备测试环境
+    from django.core.management import setup_environ
+    import wolfox.foxit.other_settings.settings_sqlite_test as settings
+    setup_environ(settings)
+
 from wolfox.fengine.normal.raw import *
+
+import logging
+logger = logging.getLogger('wolfox.fengine.normal.core.demo_test')
 
 class ModuleTest(unittest.TestCase): #保持raw()的有效性
     def test_raw(self):
@@ -21,5 +32,6 @@ class ModuleTest(unittest.TestCase): #保持raw()的有效性
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="test.log",level=logging.DEBUG,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
     unittest.main()
 

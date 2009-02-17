a529700c40c7418f5e2fc16293041d0bbaa72778
wanfox <wanfox@471201e2-bed9-11dd-b7f4-ab9653f8cbb3>
2009-02-17 02:08:42 +0000

    去除customcruiser中的debug信息

--- core/cruiser/customcruiser.py
+++ core/cruiser/customcruiser.py
@@ -31,15 +31,16 @@ class Svama3MMCruiser(MM_GeneticCruiser):
         print 'prepare:'
         self.args = dict(fast=range(1,49),mid=range(2,100),slow=range(5,260)
                 ,sma=range(3,130),ma_standard=range(5,260),extend_days=range(5,36)
-                ,munit=range(1,15))
+                )
         self.buy_func = lambda stock,fast,mid,slow,sma,ma_standard,extend_days,**kwargs:svama3(stock,fast,mid,slow,sma,ma_standard,extend_days)
         #kwargs用于吸收其它函数所需的参数
         #self.sell_func = csc_func
         self.sell_func = atr_seller
-        #self.predefined = [dict(fast=1,mid=2,slow=5,sma=3,ma_standard=5,extend_days=5)
-        #        ,dict(fast=45,mid=76,slow=85,sma=51,ma_standard=65,extend_days=5)
-        #        ,dict(fast=45,mid=76,slow=76,sma=51,ma_standard=65,extend_days=5)]
-        self.predefined = [dict(fast=15,mid=94,slow=209,sma=24,ma_standard=202,extend_days=30,munit=13)] * 16
+        self.predefined = [dict(fast=1,mid=2,slow=5,sma=3,ma_standard=5,extend_days=5)
+                ,dict(fast=45,mid=76,slow=85,sma=51,ma_standard=65,extend_days=5)
+                ,dict(fast=45,mid=76,slow=76,sma=51,ma_standard=65,extend_days=5)
+                ,dict(fast=15,mid=94,slow=209,sma=24,ma_standard=202,extend_days=30)]
+        #self.predefined = [dict(fast=15,mid=94,slow=209,sma=24,ma_standard=202,extend_days=30)]
         #self.sell_func = my_csc_func
         #self.trade_func = fcustom(normal_trade_func,begin=20010601)
         #self.trade_func = fcustom(my_trade_func,begin=20010601)
@@ -50,12 +51,12 @@ if __name__ == '__main__':
     logging.basicConfig(filename="custom_cruiser_mm.log",level=logging.DEBUG,format='#%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
 
     begin,end = 20010701,20060101
-    #dates,sdata,idata,catalogs = prepare_all(begin,end,[],[ref_code])
+    dates,sdata,idata,catalogs = prepare_all(begin,end,[],[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,['SH601988','SH600050'],[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,['SH601988'],[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,['SH600050'],[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,['SH601398'],[ref_code])
-    dates,sdata,idata,catalogs = prepare_all(begin,end,['SZ000630'],[ref_code])
+    #dates,sdata,idata,catalogs = prepare_all(begin,end,['SZ000630'],[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,get_codes(),[ref_code])
     #dates,sdata,idata,catalogs = prepare_all(begin,end,get_codes(source='SZSE'),[ref_code])
     import psyco
@@ -66,7 +67,7 @@ if __name__ == '__main__':
     d_posort('g120',sdata.values(),distance=120)     
     d_posort('g250',sdata.values(),distance=250)     
     #cruiser = Svama3MMCruiser(psize=100,maxstep=40,goal=20000)
-    cruiser = Svama3MMCruiser(psize=16,maxstep=1,goal=20000)
+    cruiser = Svama3MMCruiser(psize=100,maxstep=50,goal=20000)
     print 'before cruiser,array number:',get_obj_number(np.ndarray),',tuple number:',get_obj_number(tuple),',list number:',get_obj_number(list)
     cruiser.gcruise(sdata,dates,20010701)    
     print 'after cruiesr,array number:',get_obj_number(np.ndarray),',tuple number:',get_obj_number(tuple),',list number:',get_obj_number(list)    

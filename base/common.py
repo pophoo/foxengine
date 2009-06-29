a40948351f8ec45571b630322247127546b87c2f
wanfox <wanfox@471201e2-bed9-11dd-b7f4-ab9653f8cbb3>
2009-06-29 01:26:52 +0000

    细微调整

--- base/common.py
+++ base/common.py
@@ -107,7 +107,7 @@ class Evaluation(object):
     def header(self):
          return u'''\t评估:总盈亏值=%(balance)s,交易次数=%(count)s\t期望值=%(R)s\t%(remark)s
 \t\t总盈亏率(1/1000)=%(ratesum)s,平均盈亏率(1/1000)=%(rateavg)s,盈利交易率(1/1000)=%(winrate)s
-\t\t平均持仓时间=%(holdingavg)s,持仓效率(1/1000)=%(E)s
+\t\t平均持仓时间=%(holdingavg)s,持仓效率(1/1000000)=%(E)s
 \t\t赢利次数=%(wincount)s,赢利总值=%(winamount)s
 \t\t亏损次数=%(lostcount)s,亏损总值=%(lostamount)s
 \t\t平盘次数=%(deucecount)s\n''' % self.__dict__

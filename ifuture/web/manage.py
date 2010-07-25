5adcb81d489f4300e64f5e4fe1d9a3eaaae5769e
wanfox <wanfox@471201e2-bed9-11dd-b7f4-ab9653f8cbb3>
2010-07-25 07:01:51 +0000

    添加止损

--- ifuture/web/manage.py
+++ ifuture/web/manage.py
@@ -23,6 +23,24 @@ class LastUpdate:
         fname,sif,xactions = dynamic.whget(ifuncs.xxx)
         #return "name=%s,lastupdate=%s:%s" % (fname,sif.transaction[IDATE][-1],sif.transaction[ITIME][-1])
         lasttime = "%s-%s" % (sif.transaction[IDATE][-1],sif.transaction[ITIME][-1])
+        for action in xactions:
+            stop1 = sif.atr5x[action.index]/2000.0
+            stop2 = sif.atr[action.index]*1.5/1000
+            if stop1 < 3:
+                stop1 = 3
+            if stop2 < 3:
+                stop2 = 3
+            if action.position == LONG:
+                action.stop1 = action.price - stop1
+                action.stop2 = action.price - stop2
+                action.stop = min(action.stop1,action.stop2)
+            else:
+                action.stop1 = action.price + stop1
+                action.stop2 = action.price + stop2
+                action.stop = max(action.stop1,action.stop2)
+            action.stop1 = round(action.stop1,1)    #对空头可能多了0.05个点
+            action.stop2 = round(action.stop2,1) 
+            action.stop = round(action.stop,1) 
         return render.last(fname,lasttime,xactions)
 
 

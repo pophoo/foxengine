c84ddbfc456e69f5e6c0d590369249e20da0a100
wanfox <wanfox@471201e2-bed9-11dd-b7f4-ab9653f8cbb3>
2011-02-21 12:22:56 +0000

    2011-02-21

--- ifuture/ctp/agent.py
+++ ifuture/ctp/agent.py
@@ -7,12 +7,15 @@ Agent的目的是合并行情和交易API到一个类中进行处理
 '''
 
 import time
+import logging
 
 import UserApiStruct
 from MdApi import MdApi, MdSpi
 from TraderApi import TraderApi, TraderSpi  
 
-import logging
+from wolfox.fengine.core.base import BaseObject
+
+
 logging.basicConfig(filename="ctp_trade.log",level=logging.DEBUG,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
 
 
@@ -21,7 +24,7 @@ THOST_TERT_RESTART  = 0
 THOST_TERT_RESUME   = 1
 THOST_TERT_QUICK    = 2
 
-inst = [u'IF1102',u'IF1103',u'IF1106']
+inst = [u'IF1102',u'IF1103',u'IF1106']  #必须采用ctp使用的合约名字，内部不做检验
 #inst = [u'IF1102']
 
 
@@ -130,21 +133,7 @@ class TraderSpiDelegate(TraderSpi):
         #todo:logging
         pass
 
-    def OnRspQryExchange(self, pExchange, pRspInfo, nRequestID, bIsLast):
-        #为啥查这个?
-        pass
-
     def OnRspOrderAction(self, pInputOrderAction, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    def OnRspQryInvestor(self, pInvestor, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询投资者响应'''
-        pass
-
-    def OnRspRemoveParkedOrder(self, pRemoveParkedOrder, pRspInfo, nRequestID, bIsLast):
-        '''
-删除预埋单响应'''
         pass
 
     def OnRspQrySettlementInfo(self, pSettlementInfo, pRspInfo, nRequestID, bIsLast):
@@ -162,34 +151,14 @@ class TraderSpiDelegate(TraderSpi):
 登录请求响应'''
         pass
 
-    def OnRspParkedOrderAction(self, pParkedOrderAction, pRspInfo, nRequestID, bIsLast):
-        '''
-预埋撤单录入请求响应'''
-        pass
-
     def OnErrRtnOrderAction(self, pOrderAction, pRspInfo):
         '''
 报单操作错误回报'''
         pass
 
-    def OnRtnInstrumentStatus(self, pInstrumentStatus):
-        '''
-合约交易状态通知'''
-        pass
-
     def OnRspOrderInsert(self, pInputOrder, pRspInfo, nRequestID, bIsLast):
         '''
 报单录入请求响应'''
-        pass
-
-    def OnRspQryEWarrantOffset(self, pEWarrantOffset, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询仓单折抵信息响应'''
-        pass
-
-    def OnRspParkedOrderInsert(self, pParkedOrder, pRspInfo, nRequestID, bIsLast):
-        '''
-预埋单录入请求响应'''
         pass
 
     def OnRtnTradingNotice(self, pTradingNoticeInfo):
@@ -211,11 +180,6 @@ class TraderSpiDelegate(TraderSpi):
     def OnRspQryTradingCode(self, pTradingCode, pRspInfo, nRequestID, bIsLast):
         '''
 请求查询交易编码响应'''
-        pass
-
-    def OnRtnErrorConditionalOrder(self, pErrorConditionalOrder):
-        '''
-提示条件单校验错误'''
         pass
 
     def OnRspQrySettlementInfoConfirm(self, pSettlementInfoConfirm, pRspInfo, nRequestID, bIsLast):
@@ -243,26 +207,6 @@ class TraderSpiDelegate(TraderSpi):
 请求查询投资者持仓明细响应'''
         pass
 
-    def OnRspQryParkedOrderAction(self, pParkedOrderAction, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询预埋撤单响应'''
-        pass
-
-    def OnRspQryBrokerTradingParams(self, pBrokerTradingParams, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询经纪公司交易参数响应'''
-        pass
-
-    def OnRspQryParkedOrder(self, pParkedOrder, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询预埋单响应'''
-        pass
-
-    def OnRspQueryBankAccountMoneyByFuture(self, pReqQueryAccount, pRspInfo, nRequestID, bIsLast):
-        '''
-期货发起查询银行余额应答'''
-        pass
-
     def OnRspQueryMaxOrderVolume(self, pQueryMaxOrderVolume, pRspInfo, nRequestID, bIsLast):
         '''
 查询最大报单数量响应'''
@@ -278,16 +222,6 @@ class TraderSpiDelegate(TraderSpi):
 报单录入错误回报'''
         pass
 
-    def OnRspQryTradingNotice(self, pTradingNotice, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询交易通知响应'''
-        pass
-
-
-    def OnRspQryNotice(self, pNotice, pRspInfo, nRequestID, bIsLast):
-        '''
-请求查询客户通知响应'''
-        pass
 
     def OnRspQryTradingAccount(self, pTradingAccount, pRspInfo, nRequestID, bIsLast):
         '''
@@ -303,11 +237,6 @@ class TraderSpiDelegate(TraderSpi):
     def OnRspQryDepthMarketData(self, pDepthMarketData, pRspInfo, nRequestID, bIsLast):
         '''
 请求查询行情响应'''
-        pass
-
-    def OnRspRemoveParkedOrderAction(self, pRemoveParkedOrderAction, pRspInfo, nRequestID, bIsLast):
-        '''
-删除预埋撤单响应'''
         pass
 
     def OnFrontConnected(self, ):
@@ -334,9 +263,24 @@ class TraderSpiDelegate(TraderSpi):
 class Agent(object):
     logger = logging.getLogger('ctp.agent')
 
-    def __init__(self):
+    def __init__(self,trader):
+        '''
+            trader为交易对象
+        '''
+        self.trader = trader
         self.requestid = 1
+        self.base_funcs = []  #基本函数集合. 如合成分钟数据,30,日数据等.  需处理动态数据. 
+                              # 接口为(data,dyndata), 把dyndata添加到data的相应属性中去
+                              #顺序关系非常重要
+        self.data_funcs = []  #计算函数集合. 如计算各类指标, 顺序关系非常重要
+                              # 接口为(data), 从data的属性中取数据,并计算另外一些属性
+                              #顺序关系非常重要，否则可能会紊乱
         self.strategy_map = {}
+        self.data = {}    #为合约号==>合约数据的dict
+        self.lastupdate = 0
+        self.holding = []   #(合约、策略族、基准价、基准时间、requestid、持仓量、止损价、止损函数)
+        self.transited_orders = []    #发出后等待回报的指令, 回报后到holding
+        self.queued_orders = []     #因为保证金原因等待发出的指令(合约、策略族、基准价、基准时间(到秒))
         #self.prepare()
 
     def inc_requestid(self):
@@ -351,9 +295,9 @@ class Agent(object):
         '''
         pass
 
-    def register(self,strategys):
+    def register_strategy(self,strategys):
         '''
-            策略注册
+            策略注册. 简单版本，都按照100%用完合约分额计算. 这样，合约的某个策略集合持仓时，其它策略集合就不能再开仓
             strategys是[(合约1,策略集1),(合约2,策略集2)]的对
                 其中一个合约可以对应多个策略，一个策略也可以对应多个合约
         '''
@@ -363,7 +307,7 @@ class Agent(object):
             if s not in self.strategy_map[ins_id]:
                 self.strategy_map[ins_id].append((s,100)) 
 
-    def cregister(self,strategys):
+    def cregister_strategy(self,strategys):
         '''
             策略注册，复杂版本
             strategys是[(合约1,策略集合1,手数占比),(合约2,策略集合2,手数占比)]的集合
@@ -378,9 +322,90 @@ class Agent(object):
                 self.strategy_map[ins_id] = []
             self.strategy_map[ins_id].append((s,proportion))    #重复的话就会引发多次下单
 
+    def register_base_funcs(self,funcs):
+        self.base_funcs.update(funcs)
+    
+    def register_data_funcs(self,funcs):
+        self.data_funcs.update(funcs)
 
     def RtnMarketData(self,market_data):#行情处理主循环
+        inst = market_data.InstrumentID
+        self.prepare(market_data)
+        #先平仓
+        close_positions = self.check_close_signal()
+        self.make_trade(close_positions)
+        #再开仓.
+        open_signals = self.check_signal()
+        open_positions = self.make_position(open_signals)
+        self.make_trade(open_positions)
+        #撤单, 撤销3分钟内未成交的以及等待发出队列中30秒内未发出的单子
+        self.cancel()
+        #检查待发出单
+        self.check_queued()
+        ##扫尾
+        self.finalize()
+        
+    def prepare(self,dyndata):
+        '''
+            准备计算, 包括分钟数据、指标的计算
+        '''
+        for func in self.base_funcs:
+            func(self.data,dyndata)
+        for func in self.data_funcs:
+            func(self.data)
+
+    def check_close_signal(self):
+        '''
+            检查平仓信号
+        '''
+        return []
+
+
+    def check_signal(self):
+        '''
+            检查信号并发出指令
+            信号包括开仓信号、平仓信号
+        '''
+        return []
+
+    def make_position(self,signals):
+        '''
+            根据信号集合来确定仓位
+            signal的结构为(合约号、开/平、策略集id、开平比例)
+        '''
+        return []
+
+    def make_trade(self,positions):
+        '''
+            根据仓位指令进行交易
+            position的结构为(合约号、开/平、策略集id、开平手数)
+            必须处理同时发出开平仓指令时保证金不足引起的问题，此时，应该放入到队列中, 并在每次平仓后检查这个队列中是否满足保证金
+                这类指令必须在指定时间内(如30秒)实现，否则废弃
+        '''
         pass
+
+    def finalize(self):
+        #记录分钟数据??
+        pass
+
+    def resume(self):
+        '''
+            恢复环境
+            对每一个合约:
+                1. 获得必要的历史数据
+                2. 获得当日分钟数据, 并计算相关指标
+                3. 获得当日持仓，并初始化止损. 
+        '''
+        pass
+
+    def rtn_order(self):
+        '''
+            下单回报, 需要处理下单错误, 正常时不应该发生，因为此时必须手工干预(无法判断网络问题或计算问题或其它问题)
+        '''
+        pass
+
+    
+
 
 def md_main():
     user = MdApi.CreateMdApi("data")
@@ -397,4 +422,5 @@ def md_main():
     while True:
         time.sleep(1)
 
-if __name__=="__main__": main()
+if __name__=="__main__":
+    main()
